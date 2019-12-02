abeb20e3c0bf160292bd9aebb929d573dca3b96e
mchancan <marvin.chancan@gmail.com>
2019-12-02 18:17:38 +1000

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,85 @@
-# FlyNet
+# A Hybrid Compact Neural Architecture for Visual Place Recognition
 
-Code coming soon, visit the project page at: https://mchancan.github.io/projects/FlyNet/index.html
+![](readme/hybrid.png)
+
+In this release, we provide an open source implementation of the FlyNet supervised learning experiments
+in [A Hybrid Compact Neural Architecture for Visual Place Recognition](https://arxiv.org/pdf/1910.06840.pdf),
+as submitted to RA-L with ICRA 2020 option (https://arxiv.org/abs/1910.06840).
+
+## Abstract
+
+State-of-the-art algorithms for visual place recognition can be broadly split into two categories: computationally expensive deep-learning/image retrieval based techniques with minimal biological plausibility, and computationally cheap, biologically inspired models that yield poor performance in real-world environments. In this paper we present a new compact and high-performing system that bridges this divide for the first time. Our approach comprises two key components: FlyNet, a compact, sparse two-layer neural network inspired by fruit fly brain architectures, and a one-dimensional continuous attractor neural network (CANN). Our FlyNet+CANN network combines the compact pattern recognition capabilities of the FlyNet model with the powerful temporal filtering capabilities of an equally compact CANN, replicating entirely in a neural network implementation the functionality that yields high performance in algorithmic localization approaches like SeqSLAM. We evaluate our approach and compare it to three state-of-the-art methods on two benchmark real-world datasets with small viewpoint changes and extreme appearance variations including different times of day (afternoon to night) where it achieves an AUC performance of 87%, compared to 60% for Multi-Process Fusion, 46% for LoST-X and 1% for SeqSLAM, while being 6.5, 310, and 1.5 times faster respectively.
+
+## Datasets
+
+The dataset needed to run this code can be downloaded from
+[here](https://drive.google.com/open?id=1xrHKrHYgSqrMk9-XeC1qIe8UYDmOsgfd), which is a small subset of the Nordland dataset. This code can easily adapted to run across other, much larger datasets.
+
+
+The files contained in the repository are the following:
+
+*   `train.py` is where the training and logging loop happen; The file comes
+    with the flags defined in Table 1 of the paper. In order to run this file
+    you will need to specify where the dataset is stored and where you want to
+    save the results. The results are saved in PDF format and they contains the
+    ratemaps and the spatial autocorrelagram order by grid score. The units are
+    ordered from higher to lower grid score. Only the last evaluation is saved.
+    Please note that given random seeds results can vary between runs.
+
+*   `data_reader.py` read the TFRecord and returns a ready to use batch, which
+    is already shuffled.
+
+*   `model.py` contains the grid-cells network
+
+*   `scores.py` contains all the function for calculating the grid scores and
+    doing the plotting.
+
+*   `ensembles.py` contains the classes to generate the targets for training of
+    the grid-cell networks.
+
+## Train
+
+The implementation requires an installation of
+[TensorFlow](https://www.tensorflow.org/) version 1.12, and
+[Sonnet](https://github.com/deepmind/sonnet) version 1.27.
+
+```shell
+$ virtualenv env
+$ source env/bin/activate
+$ pip install --upgrade numpy==1.13.3
+$ pip install --upgrade tensorflow==1.12.0-rc0
+$ pip install --upgrade dm-sonnet==1.27
+$ pip install --upgrade scipy==1.0.0
+$ pip install --upgrade matplotlib==1.5.2
+$ pip install --upgrade tensorflow-probability==0.5.0
+$ pip install --upgrade wrapt==1.9.0
+```
+
+An example training script can be executed from a python interpreter:
+
+```shell
+$ python train.py --task_root='path/to/datasets/root/folder' --saver_results_directory='path/to/results/folder'
+```
+
+## License
+
+FlyNet itself is released under the MIT License (refer to the LICENSE file for details).
+
+
+## Citation
+
+If you find this project useful for your research, please use the following BibTeX entry.
+
+    @article{
+	FlyNetMC19,
+	author = {Chanc\'an, Marvin and Hernandez-Nunez, Luis and Narendra, Ajay and Barron, Andrew B. and Milford, Michael},
+	title = {A Compact Neural Architecture for Visual Place Recognition},
+	volume = {abs/1910.06840},
+	year = {2019},
+	url = {https://arxiv.org/abs/1910.06840},
+	archivePrefix = {arXiv},
+	eprint = {1910.06840},
+    	primaryClass = {cs.CV}
+}
+
+
---
+++ main.py
@@ -0,0 +1,208 @@
+from __future__ import print_function, division
+
+import cv2
+from skimage import io
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+from models import fna
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+data_dir = 'dataset/Nordland-Subset-100/'
+
+train_dir = data_dir + 'summer/' # train
+test_dir = data_dir + 'fall/' # test
+test_dir2 = data_dir + 'winter/' # test_2
+
+num_classes = 100
+img_w = 64
+img_h = 32
+img_c = 1
+
+
+# Loading the data
+
+def get_images(img_dir, num_imgs):
+    print('Loading images...')
+    x = []
+    for i in range(num_imgs):
+        img = cv2.imread(img_dir+'/'+str(i)+'.png')
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (img_w,img_h),interpolation = cv2.INTER_AREA)
+        x += [img.reshape(img_h,img_w,img_c)]
+    return np.asarray(x)/255
+
+x_train = get_images(train_dir, num_classes)
+print('Train images shape:', x_train.shape)
+
+x_test = get_images(test_dir, num_classes)
+print('Test_1 images shape:', x_test.shape)
+
+x_test2 = get_images(test_dir2, num_classes)
+print('Test_2 images shape:', x_test2.shape)
+
+
+# Nordland dataset class
+
+class NordlandDataset(Dataset):
+    """Nordland dataset."""
+    
+    def __init__(self, data):
+        """
+        Args:
+            data (string): Directory with all the images.
+        """
+        self.num_images = data.shape[0]
+        self.data = data
+    
+    def __len__(self):
+        return self.num_images
+    
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        
+        image = self.data[idx]
+        ids = np.array(idx)
+        image = image.transpose((2, 0, 1))
+        return torch.from_numpy(image), torch.from_numpy(ids)
+
+
+# Hyper parameters
+num_epochs = 200
+batch_size = num_classes
+learning_rate = 0.001
+hidden_size = 64
+iter_display = 1
+
+input_size = img_w*img_h
+
+
+test_dataset = NordlandDataset(data=x_test)
+test_loader = DataLoader(test_dataset, batch_size=batch_size,
+                         shuffle=False, num_workers=4)
+
+test_dataset2 = NordlandDataset(data=x_test2)
+test_loader2 = DataLoader(test_dataset2, batch_size=batch_size,
+                         shuffle=False, num_workers=4)
+
+train_dataset = NordlandDataset(data=x_train)
+dataloader = DataLoader(train_dataset, batch_size=batch_size,
+                        shuffle=False, num_workers=4)
+
+
+# FlyNet model, including a hidden-layer (FNA) and a fully-connected (FC) output layer
+
+class FlyNet(nn.Module):
+    def __init__(self, input_size=input_size, hidden_size=hidden_size, num_classes=num_classes):
+        super(FlyNet, self).__init__()
+        self.sampling_ratio = 0.1 # 10% random, sparse connectivity
+        self.wta_length = int(hidden_size/2) # 50% WTA
+        self.fna_weight = torch.Tensor(input_size, hidden_size)
+        self.fc = nn.Linear(hidden_size, num_classes, bias=False)
+        self.reset_fna_weight()
+        
+    def reset_fna_weight(self):
+        # Defining W: binary, sparse matrix
+        self.fna_weight = (nn.init.sparse_(self.fna_weight,
+                                           sparsity=1-self.sampling_ratio)!=0).float().to(device)
+    def fna(self, x):
+        firing_rates = torch.matmul(x,self.fna_weight)
+        wta_threshold = torch.topk(firing_rates, self.wta_length, dim=1)[0][:,-1].reshape(num_classes,1)
+        return (firing_rates>=wta_threshold).float()
+        
+    def forward(self, x):
+        out = self.fna(x)
+        out = self.fc(out)
+        return out
+
+model = FlyNet().to(device)
+
+# Loss and optimizer
+criterion = nn.CrossEntropyLoss()  
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+
+# Train the model
+
+total_step = len(dataloader)
+
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(dataloader):
+        images = images.view(images.size(0), -1).to(device)
+        labels = labels.to(device)
+        
+        # Forward pass
+        outputs = model(images.float())
+        loss = criterion(outputs, labels)
+        
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        # Compute accuracy
+        _, argmax = torch.max(outputs, 1)
+        accuracy = (labels == argmax.squeeze()).float().mean()
+    
+    if (epoch+1) % iter_display == 0:
+        print ('Epoch [{}/{}], train_loss: {:.4f}, train_acc: {:.2f}'
+                .format(epoch+1, num_epochs, loss.item(), accuracy.item()))
+
+
+# Test the model
+
+def eval_model(model,data_loader):
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for images, labels in data_loader:
+            images = images.view(images.size(0), -1).to(device)
+            labels = labels.to(device)
+            outputs = model(images.float())
+            
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+        print('Test Accuracy: {} %'.format(100 * correct / total))
+    return outputs.data.cpu().numpy().argmax(axis=1)
+
+
+y0 = eval_model(model, dataloader)
+y1 = eval_model(model, test_loader)
+y2 = eval_model(model, test_loader2)
+
+
+# Plot the resutls
+
+plt.figure(figsize=(15,3))
+num_samples = 10
+samples_per_class = 3
+tol = 11
+
+idxs = np.random.choice(num_classes, num_samples, replace=False)
+
+for cls in range(num_samples):
+    idx = idxs[cls]
+    for i in range(samples_per_class):
+        plt_idx = i * num_samples + cls + 1
+        plt.subplot(samples_per_class, num_samples, plt_idx)
+        if i == 0:
+            plt.imshow(io.imread(train_dir+f'{y0[idx]}.png'))
+            plt.title(f'{y0[idx]}', color='blue' if abs(idx-y0[idx])<tol else 'red')
+        elif i == 1:
+            plt.imshow(io.imread(test_dir+f'{y1[idx]}.png'))
+            plt.title(f'{y1[idx]}', color='blue' if abs(idx-y1[idx])<tol else 'red')
+        else:
+            plt.imshow(io.imread(test_dir2+f'{y2[idx]}.png'))
+            plt.title(f'{y2[idx]}', color='blue' if abs(idx-y2[idx])<tol else 'red')
+        plt.axis('off')
+plt.tight_layout()
+plt.savefig('eval_flynet.jpg', dpi = 400)
+
